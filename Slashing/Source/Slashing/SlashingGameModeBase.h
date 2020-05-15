3c8a78bc71821de95861e8d43e4cb561f8e1f667
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-05-15 03:38:35 -0700

    Fresh start to a hack and slash project

---
+++ Slashing/Source/Slashing/SlashingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlashingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASHING_API ASlashingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
