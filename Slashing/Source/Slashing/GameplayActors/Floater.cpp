24e72146abdfee53203e34ee0dbc7ac9c010446d
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-05-18 02:18:35 -0700

    updated assets

---
+++ Slashing/Source/Slashing/GameplayActors/Floater.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Floater.h"
+
+// Sets default values
+AFloater::AFloater()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CustomStaticMesh"));
+	InitialLocation = FVector(0.0f);
+	PlacedLocation = FVector(0.0f);
+
+	WorldOrigin = FVector(0.0f, 0.0f, 0.0f);
+	InitialDirection = FVector(0.0f, 0.0f, 0.0f);
+
+	bInitializeFloaterLocations = false;
+	bShouldFloat = false;
+}
+
+// Called when the game starts or when spawned
+void AFloater::BeginPlay()
+{
+	Super::BeginPlay();
+	PlacedLocation = GetActorLocation();
+	if (bInitializeFloaterLocations)
+	{
+		SetActorLocation(InitialLocation);
+	}
+	FHitResult HitResult;
+	FVector LocalOffset = FVector(200.f, 0.0f, 0.0f);
+	AddActorWorldOffset(LocalOffset, true, &HitResult);
+	
+}
+
+// Called every frame
+void AFloater::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bShouldFloat)
+	{
+		FHitResult HitResult;
+		AddActorLocalOffset(InitialDirection, true, &HitResult);
+
+		FVector HitLocation = HitResult.Location;
+
+	}
+}
+
---
+++ Slashing/Source/Slashing/GameplayActors/Floater.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Floater.generated.h"
+
+UCLASS()
+class SLASHING_API AFloater : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloater();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ActorMeshComponents")
+	UStaticMeshComponent* StaticMesh;
+
+	// Location used by SetActorLocation() when BeginPlay() is called
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Floater Variables")
+	FVector InitialLocation;
+
+	// Locaion of the Actor when dragged in from the editor
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "Floater Variables")
+	FVector PlacedLocation;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Floater Variables")
+	FVector WorldOrigin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Floater Variables")
+	FVector InitialDirection;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Floater Variables")
+	bool bShouldFloat;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Floater Variables")
+	bool bInitializeFloaterLocations;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
