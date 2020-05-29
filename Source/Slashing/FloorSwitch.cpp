9c5dadb85d7cce058aa5ac0587299eab379ab1f8
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-05-28 18:51:21 -0700

    added starting content and begin working on first gameplay mechanic, floor switch

--- Source/Slashing/FloorSwitch.cpp
+++ Source/Slashing/FloorSwitch.cpp
@@ -2,6 +2,8 @@
 
 
 #include "FloorSwitch.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AFloorSwitch::AFloorSwitch()
@@ -9,13 +11,32 @@ AFloorSwitch::AFloorSwitch()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	RootComponent = TriggerBox;
+
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	TriggerBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	TriggerBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	TriggerBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	TriggerBox->SetBoxExtent(FVector(62.f, 62.f, 32.f));
+
+	FloorSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorSwitch"));
+	FloorSwitch->SetupAttachment(GetRootComponent());
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	Door->SetupAttachment(GetRootComponent());
+
 }
 
 // Called when the game starts or when spawned
 void AFloorSwitch::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapBegin);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapEnd);
+
 }
 
 // Called every frame
@@ -24,4 +45,15 @@ void AFloorSwitch::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void AFloorSwitch::OnOverlapBegin(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Component Begin Overlapping"));
+}
+
+void AFloorSwitch::OnOverlapEnd(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Component End Overlapping"));
+}
+
 
--- Source/Slashing/FloorSwitch.h
+++ Source/Slashing/FloorSwitch.h
@@ -16,14 +16,17 @@ public:
 	AFloorSwitch();
 
 	/** Overlap volume for the functionality to be triggered */
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite. Category = "Floor Switch")
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
 	class UBoxComponent* TriggerBox;
 
 	/** Switch for the character to step on*/
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite.Category = "Floor Switch")
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
 	class UStaticMeshComponent* FloorSwitch;
 
-	/**  */
+	/** Door to move when the floor switch stepped on */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
+	UStaticMeshComponent* Door;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -32,4 +35,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
