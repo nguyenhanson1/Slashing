57bbb8afc8de5b295c2846dab6589fe5869e6569
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-05-30 18:36:14 -0700

    Created Spawning volume for map

--- Source/Slashing/SpawnVolume.h
+++ Source/Slashing/SpawnVolume.h
@@ -15,6 +15,13 @@ public:
 	// Sets default values for this actor's properties
 	ASpawnVolume();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
+	class UBoxComponent* SpawningBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	TSubclassOf<class ACritter> PawnToSpawn;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,4 +30,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+	FVector GetSpawnPoint();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Spawning")
+	void SpawnPawn(UClass* ToSpawn, const FVector& Location);
 };
